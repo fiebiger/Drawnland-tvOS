a7ab74d3894fd95ba9f38ad0333c50785374c71c
Matheo Fiebiger <matheof@hotmail.com>
2016-04-09 11:37:35 +0200

    Adding more shitz

--- Drawland/Drawland/AppDelegate.swift
+++ Drawland/Drawland/AppDelegate.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import GameController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
@@ -41,6 +42,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
--- Drawland/Drawland/ViewController.swift
+++ Drawland/Drawland/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import GameController
+
+class TouchTrackerView: UIView
+{
+    lazy var path: UIBezierPath = {
+        let path = UIBezierPath()
+        
+        path.lineWidth = 4.0
+        
+        return path
+    }()
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch: UITouch = touches.first!
+        path.moveToPoint(touch.locationInView(self))
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch: UITouch = touches.first!
+        self.path.addLineToPoint(touch.locationInView(self))
+        self.setNeedsDisplay()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch: UITouch = touches.first!
+        path.addLineToPoint(touch.locationInView(self))
+        self.setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        self.touchesEnded(touches!, withEvent: event)
+    }
+    
+    override func drawRect(rect: CGRect) {
+        path.stroke()
+    }
+    
+    override func canBecomeFocused() -> Bool {
+        return true
+    }
+    
+    override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+        
+        if context.nextFocusedView == self {
+            
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.backgroundColor = UIColor.blueColor().colorWithAlphaComponent(0.2)
+                
+                }, completion: nil)
+            
+        } else if context.previouslyFocusedView == self {
+            
+            coordinator.addCoordinatedAnimations({ () -> Void in
+                
+                self.backgroundColor = UIColor.redColor()
+                
+                }, completion: nil)
+            
+        }
+        
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
